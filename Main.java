d4ec32bf23311f0adfecafaf528c3c497f4472d9
Michail Cook <michailcook@gmail.com>
2019-12-02 15:37:49 -0600

    Added methods for slider and for adding elements to scene

--- Main.java
+++ Main.java
@@ -10,13 +10,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 
 	Slider hammingDistSlider;
+	Scene windowContent;
+	HBox windowLayout = new HBox(20);
+	TextField prompt;
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,6 +33,9 @@ public class Main extends Application {
 	public void start(Stage projectWindow) throws Exception {
 		// TODO Auto-generated method stub
 		projectWindow.setTitle("Project 5");
+		createSlider();
+		addElementsToWindow();
+		projectWindow.setScene(windowContent);
 		projectWindow.show();
 		
 			
@@ -35,11 +44,22 @@ public class Main extends Application {
 		
 	}
 	
-	/*
-	 * Method that creates the slider for the 
+	/**
+	 * Method that adds JavaFx objects to the Scene 
+	 */
+	public void addElementsToWindow() {
+		
+		windowContent = new Scene(windowLayout, 500, 500);
+		windowLayout.getChildren().add(prompt);
+		windowLayout.getChildren().add(hammingDistSlider);
+	}
+	
+	/**
+	 * Method that creates the slider for the Hamming Dist of stations
+	 *  
 	 */
 	public void createSlider() {
-		TextField prompt = new TextField("Enter Hamming Distance.");
+		prompt = new TextField("Enter Hamming Distance.");
 		prompt.setEditable(false);
 		
 		hammingDistSlider = new Slider(1,5,3);
@@ -50,7 +70,6 @@ public class Main extends Application {
 		hammingDistSlider.setBlockIncrement(1);
 		hammingDistSlider.setSnapToTicks(true);
 		hammingDistSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
 				// TODO Auto-generated method stub
