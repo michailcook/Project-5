ed821467e084176016f417a37464267aa2914362
Michail Cook <michailcook@gmail.com>
2019-12-02 15:11:16 -0600

    Set up basic design and function of the slider

--- Main.java
+++ Main.java
@@ -8,17 +8,15 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-//Stage is the window
-//Scene is the content inside
 
 public class Main extends Application {
 
-	Button button;
-	Slider slider;
+	Slider hammingDistSlider;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -29,44 +27,40 @@ public class Main extends Application {
 	public void start(Stage projectWindow) throws Exception {
 		// TODO Auto-generated method stub
 		projectWindow.setTitle("Project 5");
+		projectWindow.show();
 		
-		button = new Button();
-		button.setText("Click me");
-		//you can set eventHandlers on the object that you made
-		button.setOnAction(new EventHandler<ActionEvent>(){
-
-		@Override
-		public void handle(ActionEvent arg0) {
-			// TODO Auto-generated method stub
 			
-		}
-		});
 		
-		slider = new Slider(1,5,3);
-		slider.setShowTickLabels(true);
-		slider.setShowTickMarks(true);
-		slider.setMajorTickUnit(1);
-		slider.setMinorTickCount(0);
-		slider.setBlockIncrement(1);
-		slider.setSnapToTicks(true);
-		//This listener shows a change in slider values
-		slider.valueProperty().addListener(new ChangeListener<Number>() {
+		
+		
+	}
+	
+	/*
+	 * Method that creates the slider for the 
+	 */
+	public void createSlider() {
+		TextField prompt = new TextField("Enter Hamming Distance.");
+		prompt.setEditable(false);
+		
+		hammingDistSlider = new Slider(1,5,3);
+		hammingDistSlider.setShowTickLabels(true);
+		hammingDistSlider.setShowTickMarks(true);
+		hammingDistSlider.setMajorTickUnit(1);
+		hammingDistSlider.setMinorTickCount(0);
+		hammingDistSlider.setBlockIncrement(1);
+		hammingDistSlider.setSnapToTicks(true);
+		hammingDistSlider.valueProperty().addListener(new ChangeListener<Number>() {
 
 			@Override
 			public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
 				// TODO Auto-generated method stub
-				System.out.println("value: " + newVal.intValue());
+				prompt.setText("Hamming Distance is " + newVal.intValue());
+				
 				
 			}
 		});
-
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		layout.getChildren().add(slider);
 		
-		Scene scene = new Scene(layout, 500, 500);
-		projectWindow.setScene(scene);
-		projectWindow.show();
+
 		
 	}
 
