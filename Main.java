50170703f08b40575781c0b540d9b695542b76e0
Michail Cook <michailcook@gmail.com>
2019-12-02 14:51:48 -0600

    More JVFX Practice, will use this as the driver for the project

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+import java.awt.Event;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -12,6 +18,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	Button button;
+	Slider slider;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -25,9 +32,37 @@ public class Main extends Application {
 		
 		button = new Button();
 		button.setText("Click me");
+		//you can set eventHandlers on the object that you made
+		button.setOnAction(new EventHandler<ActionEvent>(){
+
+		@Override
+		public void handle(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		});
 		
+		slider = new Slider(1,5,3);
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setMajorTickUnit(1);
+		slider.setMinorTickCount(0);
+		slider.setBlockIncrement(1);
+		slider.setSnapToTicks(true);
+		//This listener shows a change in slider values
+		slider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
+				// TODO Auto-generated method stub
+				System.out.println("value: " + newVal.intValue());
+				
+			}
+		});
+
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
+		layout.getChildren().add(slider);
 		
 		Scene scene = new Scene(layout, 500, 500);
 		projectWindow.setScene(scene);
